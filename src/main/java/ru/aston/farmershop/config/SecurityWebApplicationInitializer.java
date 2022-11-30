377829628c5c07bb793153ad0bb394056305e2a7
UnimaginableCat <CyberEgor4>
2022-11-30 11:18:43 +0300

    Перенос проекта на Spring boot

--- src/main/java/ru/aston/farmershop/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package ru.aston.farmershop.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- src/main/java/ru/aston/farmershop/controllers/AuthController.java
+++
@@ -1,4 +0,0 @@
-package ru.aston.farmershop.controllers;
-
-public class AuthController {
-}
