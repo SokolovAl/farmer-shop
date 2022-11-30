fed5e7e3895b9629a9b1989aeec20c00f7371b7b
UnimaginableCat <CyberEgor4>
2022-11-30 22:26:35 +0300

    Добавлена логика работы регистрации нового юзера

--- src/main/java/ru/aston/farmershop/FarmerShopApplication.java
+++ src/main/java/ru/aston/farmershop/FarmerShopApplication.java
@@ -2,8 +2,9 @@ package ru.aston.farmershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class FarmerShopApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ru/aston/farmershop/services/UserService.java
@@ -0,0 +1,10 @@
+package ru.aston.farmershop.services;
+
+import ru.aston.farmershop.dto.UserDto;
+
+public interface UserService {
+
+
+  void registerUser(UserDto userDto);
+
+}
